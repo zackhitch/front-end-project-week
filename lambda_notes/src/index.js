39f0da082113a2e55fb247d93eb3cf5af534de0f
zackhitch <zmhitchcock+git@gmail.com>
2018-09-24 13:05:09 -0700

    organization

--- lambda_notes/src/index.js
+++ lambda_notes/src/index.js
@@ -1,6 +1,31 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
+import thunk from 'redux-thunk';
+import logger from 'redux-logger';
+import { createStore, applyMiddleware, compose } from 'redux';
+import { Provider } from 'react-redux';
+import { BrowserRouter as Router } from 'react-router-dom';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+import './index.css';
+import App from './components/App/App';
+import rootReducer from './reducers';
+
+const reduxDevTools =
+  window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__();
+
+const store = createStore(
+  rootReducer,
+  compose(
+    applyMiddleware(thunk, logger),
+    reduxDevTools
+  )
+);
+
+ReactDOM.render(
+  <Provider store={store}>
+    <Router>
+      <App />
+    </Router>
+  </Provider>,
+  document.getElementById('root')
+);
---
+++ lambda_notes/src/reducers/index.js
@@ -0,0 +1,8 @@
+import { combineReducers } from 'redux';
+import { notesReducer } from './notesReducer';
+
+const rootReducer = combineReducers({
+  notesReducer,
+});
+
+export default rootReducer;
